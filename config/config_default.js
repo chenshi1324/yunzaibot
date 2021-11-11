6c5536e6500ad260512d1ebdf01d5dca8890fbfe
Le-niao <805475874@qq.com>
2021-11-11 08:57:33 +0800

    更新config

--- config/config_default.js
+++ config/config_default.js
@@ -18,6 +18,18 @@ let config = {
     db: 0,            //redis数据库
   },
 
+  //米游社cookies，支持多个
+  //访问米游社原神社区，登录后账户点击 F12 ，
+  //选中 Console 或控制台，输入 document.cookie ，回车即可获取
+  //例如ltoken=***;ltuid=***;
+  //cookie逻辑：一个号一天只能查30个uid（包括查询失败的），在没超过30的时候，之前查过的uid可以重复查询
+  //所有项目限制一个cookie只能查27个，避免超出限制，查询过的可以重复查
+  //
+  mysCookies: [
+    // "ltoken=***;ltuid=***",
+    "",
+  ],
+
   //群设置 
   group:{
     //通用默认配置
@@ -37,19 +49,6 @@ let config = {
     //   mysUidLimit: 3, //每天查询uid个数
     // }
   },
-
-  //米游社cookies，支持多个
-  //访问米游社原神社区，登录后账户点击 F12 ，
-  //选中 Console 或控制台，输入 document.cookie ，回车即可获取
-  //例如ltoken=***;ltuid=***;
-  //cookie逻辑：一个号一天只能查30个uid（包括查询失败的），在没超过30的时候，之前查过的uid可以重复查询
-  //所有项目限制一个cookie只能查27个，避免超出限制，查询过的可以重复查
-  //
-  mysCookies: [
-    // "ltoken=***;ltuid=***",
-    "",
-  ],
-
 
 };
 
