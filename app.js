f7a181942878351568581f946de4293890ddb54b
Le-niao <805475874@qq.com>
2021-12-09 17:00:05 +0800

    代码优化

--- app.js
+++ app.js
@@ -6,18 +6,17 @@ import { check } from "./lib/check.js";
 
 const packageJson = JSON.parse(fs.readFileSync("package.json", "utf8"));
 await check();
-const Bot = createClient(config.account.qq);
-global.logger = Bot.logger;
+const Bot = createClient(BotConfig.account.qq);
 global.Bot = Bot;
 
 //监听上线事件
 Bot.on("system.online", async () => {
-  logger.mark("----------");
-  logger.mark("初始化Yunzai-Bot");
+  Bot.logger.mark("----------");
+  Bot.logger.mark("初始化Yunzai-Bot");
   await init();
-  logger.mark(`Yunzai-Bot 上线成功 版本v${packageJson.version}`);
-  logger.mark("https://github.com/Le-niao/Yunzai-Bot");
-  logger.mark("----------");
+  Bot.logger.mark(`Yunzai-Bot 上线成功 版本v${packageJson.version}`);
+  Bot.logger.mark("https://github.com/Le-niao/Yunzai-Bot");
+  Bot.logger.mark("----------");
 });
 
 //监听消息并回复
@@ -34,7 +33,7 @@ Bot.on("message.private", (event) => {
 
 //处理好友事件
 Bot.on("request.friend", (event) => {
-  logger.mark(`添加好友：${event.user_id}`);
+  this.logger.mark(`添加好友：${event.user_id}`);
   Bot.setFriendAddRequest(event.flag, true);
 });
 
@@ -43,4 +42,4 @@ Bot.on("system.login.qrcode", function (e) {
   process.stdin.once("data", () => {
     this.login();
   });
-}).login(config.account.pwd);
+}).login(BotConfig.account.pwd);
