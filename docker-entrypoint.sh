631e4434fb21c3737ef90b5e9ee192f3beb1b038
Xm798 <git@xm.mk>
2022-04-30 18:18:09 +0800

    调整 docker 文件
    - 增加需要映射的目录
    - 增加失败重启机制
    - 重启自动更新后显示当前版本信息

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,12 @@ set -e
 
 echo -e "\n ======== \n ${Info} ${GreenBG} 拉取最新项目 ${Font} \n ======== \n"
 git pull
+
+echo -e "\n ======== \n ${Info} ${GreenBG} 当前版本信息 ${Font} \n ======== \n"
+git log -1 --pretty=format:"%h - %an, %ar (%cd) : %s"
+
 echo -e "\n ======== \n ${Info} ${GreenBG} 更新运行依赖 ${Font} \n ======== \n"
 cnpm install
+
 echo -e "\n ======== \n ${Info} ${GreenBG} 启动云崽-BOT ${Font} \n ======== \n"
 node app
